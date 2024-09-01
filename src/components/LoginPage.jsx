153e2ccc6dfe87cc0b34711ca659994e8b9ca64c
Gopi2923 <dgopidhikshan@gmail.com>
2024-09-01 22:58:06 +0530

    added logo.

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from "./../assets/logo-icon.jpeg"
 import './LoginPage.css';
 
 function LoginPage() {
   return (
     <div className="login-container">
       <div className="login-box">
-        <img src="/logo.png" alt="Logo" className="logo" />
+        <img src={logo} alt="Logo" className="logo" />
         <h2>Login</h2>
         <form>
           <div className="input-group">
