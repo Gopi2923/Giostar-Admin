178857e07f4b023d23f0e7ccdf1a30cec109c668
Gopi2923 <dgopidhikshan@gmail.com>
2024-09-01 22:53:46 +0530

    first commit

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './LoginPage.css';
+
+function LoginPage() {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <img src="/logo.png" alt="Logo" className="logo" />
+        <h2>Login</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
